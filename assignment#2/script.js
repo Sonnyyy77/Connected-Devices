f1e2b7c021f452efa566a1cf63528b4ccd32c2f4
Sonny <yy2627@nyu.edu>
2024-02-07 00:58:09 -0500

    Add a button to change background color

--- assignment#2/script.js
+++ assignment#2/script.js
@@ -1,22 +1,48 @@
 let slider = document.getElementById("mySlider");
 let text = document.getElementById("text");
 let roundswitch = document.getElementById("roundswitch");
+let hidtext = document.getElementById("hidtext");
+let roundbutton = document.getElementById("roundbutton");
 
 function sliderUpdated() {
-  text.style.letterSpacing = slider.value + "px";
+    text.style.letterSpacing = slider.value + "px";
 }
 
 function changecolor() {
     if (roundswitch.checked){
-        document.body.style.backgroundColor = "black";
+        document.body.style.background = "black";
         text.style.color = "white";
+        hidtext.style.opacity = "1";
     }
     else {
-        document.body.style.backgroundColor = "white";
+        document.body.style.background = "white";
         text.style.color = "black";
+        hidtext.style.opacity = "0";
     }
+}
+
+function createHex() {
+    var hexCode1 = "";
+    var hexValues1 = "0123456789abcdef";
+    
+    for ( var i = 0; i < 6; i++ ) {
+      hexCode1 += hexValues1.charAt(Math.floor(Math.random() * hexValues1.length));
+    }
+    return hexCode1;
+  }
+
+function surprise() {
+  
+    var deg = Math.floor(Math.random() *360);
+        
+    var gradient = "linear-gradient(" + deg + "deg, " + "#" + createHex() + ", " + "#" + createHex() +")";
+
+    document.body.style.background = gradient;
+        
+    console.log(hexCode1, hexCode2); 
 }
 
 sliderUpdated();
 slider.addEventListener("input", sliderUpdated);
-roundswitch.addEventListener("input", changecolor)
+roundswitch.addEventListener("input", changecolor);
+roundbutton.addEventListener("click", surprise);
