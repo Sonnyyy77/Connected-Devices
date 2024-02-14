d6870799e587f1327c65d59de791057334635fd6
Sonny <yy2627@nyu.edu>
2024-02-14 18:05:37 -0500

    Update IP Address

--- assignment3/sketch_feb14b/config.h
+++ assignment3/sketch_feb14b/config.h
@@ -1,4 +1,4 @@
-// const char WIFI_SSID[] = "sandbox370"; //WiFi network name goes here
-// const char WIFI_PASS[] = "+s0a+s03!2gether?"; //WiFi password goes here
-const char WIFI_SSID[] = "Verizon_HZWY66"; //WiFi network name goes here
-const char WIFI_PASS[] = "obtuse9-aye-dub"; //WiFi password goes here
+const char WIFI_SSID[] = "sandbox370"; //WiFi network name goes here
+const char WIFI_PASS[] = "+s0a+s03!2gether?"; //WiFi password goes here
+// const char WIFI_SSID[] = "Verizon_HZWY66"; //WiFi network name goes here
+// const char WIFI_PASS[] = "obtuse9-aye-dub"; //WiFi password goes here
