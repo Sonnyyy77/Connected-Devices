79755a6593159bb27e1b306f23fe4c4401b11b35
Sonny <yy2627@nyu.edu>
2024-04-10 17:35:07 -0400

    Update0410

--- assignment6/assignment6.ino
+++ assignment6/assignment6.ino
@@ -1,4 +1,5 @@
 #include <ArduinoHttpClient.h>
+#include <Arduino_LSM6DS3.h>
 #include <WiFiNINA.h>
 #include "arduino_secrets.h"
 
@@ -10,12 +11,26 @@ char pass[] = SECRET_PASS;
 char serverAddress[] = "147.182.142.189";  //server IP address
 int port = 1880; //use HTTP default port 80, or use 1880 for node-red
 
+float x, y, z;
+String message;
+
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, serverAddress, port);
 int status = WL_IDLE_STATUS;
 
 void setup() {
   Serial.begin(9600);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+
   while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);                   
@@ -36,11 +51,31 @@ void setup() {
 void loop() {
   Serial.println("making POST request");
 
+    if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+    
+    if(x > 0.1){
+      message = "TURNING DOWN";
+    }
+  if(x < -0.1){
+    message = "TURNING UP";
+    }
+  if(y > 0.1){
+    message = "TURNING LEFT";
+    }
+  if(y < -0.1){
+    message = "TURNING RIGHT";
+    }
+    
+    delay(100);
+  }
+
   //if sending raw text:
  String contentType = "text/plain";
 //  String postData = "hello from Arduino"; //to send a string, or
- String postData = String(millis()); //to send a number
- delay(1000);
+//  String postData = String(millis()); //to send a number
+ String postData = message;
+ delay(300);
 
   //if sending a formatted JSON object (useful for updating multiple values)
   // String contentType = "application/json"; //if sending a formatted JSON object
